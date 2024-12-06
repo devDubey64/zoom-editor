f0f867f4c22f86d18427d8708b2e5448e0d76dc0
devDubey64 <anubhavd008@gmail.com>
2024-12-06 14:36:07 +0530

    [setup] zoom-editor: Initial directory structure, dependencies

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Zoom Editor initial setup!</h1>
+        </div>
+    );
+};
+
+export default App;
